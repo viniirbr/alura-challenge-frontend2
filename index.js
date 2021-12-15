d6c8127afcd3ee7c5413053531be248a3e9c1400
Vinicius Ribeiro <vayne99@gmail.com>
2021-12-15 16:42:44 -0300

    implementação do destaque de sintaxe

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const border = document.querySelector('.fundo-editor')
-const input = document.querySelector('.input-color')
+const inputColor = document.querySelector('.input-color')
 
 setColor()
-input.addEventListener('input', setColor)
+inputColor.addEventListener('input', setColor)
 
 function setColor() {
-  border.style.background = input.value
+  border.style.background = inputColor.value
 }
+
+const linguagem = document.querySelector('.personalizacao-select')
+const areaCodigo = document.querySelector('.codigo-wrapper')
+const botao = document.querySelector('.botao-visualizarMarcacao')
+
+function aplicarHighlight() {
+	const codigo = areaCodigo.innerText
+	areaCodigo.innerHTML = `<code class="editor-codigo hljs ${linguagem.value}" contenteditable="true"></code>`
+	areaCodigo.querySelector('code').textContent = codigo
+	hljs.highlightElement(areaCodigo.querySelector('code'))
+	console.log("clicou")
+}
+
+botao.addEventListener('click', ()=> {
+	aplicarHighlight()
+})
